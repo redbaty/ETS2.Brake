d04fc63073a5271f41ab4408696ec72bc74405cd
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-06-12 17:05:05 -0300

    Added a ByPercentage overload

--- ETS2.Brake/Utils/Math.cs
+++ ETS2.Brake/Utils/Math.cs
@@ -1,7 +1,11 @@
-﻿namespace ETS2.Brake.Utils
+﻿using ETS2.Brake.Managers;
+
+namespace ETS2.Brake.Utils
 {
     internal static class Math
     {
         public static int ByPercentage(decimal percentage, decimal max) => (int) (percentage * max / 100);
+
+        public static int ByPercentage(int percentage) => Math.ByPercentage(percentage, JoystickManager.MaxValue);
     }
 }
